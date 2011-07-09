cb9f0cdd03d6038233731a223bb0f1f893241827
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2011-07-10 06:38:50 +0900

    add chksum.rb

---
+++ standard/chksum.rb
@@ -0,0 +1,18 @@
+total = 0
+sub_total = 0
+while line = gets
+  line.chomp!
+  if /x\s(\d+)$/ =~ line
+    puts line
+    sub_total += $1.to_i
+    total += $1.to_i
+  elsif /^-/ =~ line
+    puts "#{line} x 1?"
+    sub_total += 1
+    total += 1
+  elsif sub_total > 0
+    puts sub_total
+    sub_total = 0
+  end
+end
+puts "total: #{total}"
